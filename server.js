688c13f6604f3a35c270999c3e294f3a2bd20f95
Charlie Leathers <cleathers89@gmail.com>
2017-07-24 22:12:32 -0700

    added logger

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+console.log(`initializing server...`);
+
 const Koa = require('koa');
 const app = new Koa();
 const router = require('./routes');
+const logger = require('koa-logger');
+
+app.use(logger());
 
 const PUBLIC_DIR = process.env.PUBLIC_DIR || __dirname + '/public';
-
 app.use(require('koa-static')(PUBLIC_DIR))
-
 
 app.use(router.routes());
 
-app.listen(8080);
+const PORT = 8080;
+
+app.listen(PORT);
+console.log(`server listening on port ${PORT}`);
