cea52a02c63d1c879701388f922ffc4cab93303b
Charlie Leathers <cleathers89@gmail.com>
2017-07-29 22:08:15 -0700

    adds public addresses and styling of footer, adds startDev script

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,9 @@ export default class Footer extends Component {
 	render() {
 		return <footer className="page-footer grey lighten-4">
 			<div className="footer-copyright">
-				<div className="container">
-					<a class="grey-text text-lighten-4 right">ETH: xoijwef82</a>
-					<a class="grey-text text-lighten-4 right">BTC: xoijwef82</a>
-					<a class="grey-text text-lighten-4 right">LTC: xoijwef82</a>
-				</div>
+				<a><img src="https://files.coinmarketcap.com/static/img/coins/32x32/ethereum.png" />0x167df50a9f1d7e94a95876d9ad132b084456c307</a>
+				<a><img src="https://files.coinmarketcap.com/static/img/coins/32x32/bitcoin.png" />13FSTpxNNnbtRbQwkNzgbbudm2EyZ9iKqa</a>
+				<a><img src="https://files.coinmarketcap.com/static/img/coins/32x32/litecoin.png" />LbUtP4T6zHZHVYBs9GYb5iQ9se1MM75Zox</a>
 			</div>
 		</footer>
 	}
